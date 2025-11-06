fa9c1a3604a4f8f4ac20cf189379431eb955ba19
samoylovaVD <samoylova349@gmail.com>
2025-11-06 23:32:04 +0500

    Create DogBreedParser.java

---
+++ src/main/java/parser/DogBreedParser.java
@@ -0,0 +1,13 @@
+package parser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Document;
+import java.io.IOExeption;
+
+public class DogParser(){
+  privat static final String base_url = "https://www.purina.ru";
+}
+
+//4 шаг: далее пойдет создание метода парсдог. аргумент будет урл в формате строка пжпжп
+ //5 : также трай кэч будем юзать еще чтобы все не сломалось к #####
