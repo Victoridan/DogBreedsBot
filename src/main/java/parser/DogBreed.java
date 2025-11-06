9fab64b9f6ebf849fc9e58f2790806be3f6a773b
samoylovaVD <samoylova349@gmail.com>
2025-11-06 23:01:57 +0500

    Create DogBreed.java

---
+++ src/main/java/parser/DogBreed.java
@@ -0,0 +1,15 @@
+package parser;
+
+public class DogBreed{
+  
+  private String name;
+  private String description;
+
+  public DogBreed(){}
+
+  public String getName() {return name;}
+  public void setName(String name) {this.name = name;}
+                                    
+  public String getDescription() {return name;}
+  public void setDescription(String description) { this.description = description;}
+}
