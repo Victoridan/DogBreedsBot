f0f57803df9d01e6aee3ea8d681b97ff2c31f42a
samoylovaVD <samoylova349@gmail.com>
2025-11-08 01:58:50 +0500

    Update DogBreedParser.java

--- src/main/java/parser/DogBreedParser.java
+++ src/main/java/parser/DogBreedParser.java
@@ -3,11 +3,30 @@ package parser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Document;
-import java.io.IOExeption;
+import java.io.IOException;
 
-public class DogParser(){
-  privat static final String base_url = "https://www.purina.ru";
+public class DogParser{
+  private static final String base_url = "https://www.purina.ru";
+
+public DogBreed parseDog(String url){
+  try{
+    Document doc = Jsoup.connect(url).get();
+    DogBreed breed = new DogBreed();
+    Element namebreedName = doc.selectFirst("#block-ttt-mainpagecontent > div > div.breed-details > div.component-wrapper > div > div > div > div > div.hero-small--content-area-wrapper > div.hero-small--content-area.hero-small--breed.hero-small--breed__dog > div > h1");
+    if (name != 0){
+      String name = breedName.text()
+        breed.setName(name);
+        return breed;
+      
+    }else{
+      System.out.println("Не удалось найти название породы((");
+    }
+    
+  }catch(IOException e){
+    e.printStackTrace();
+    return null;
+  }
 }
-
 //4 шаг: далее пойдет создание метода парсдог. аргумент будет урл в формате строка пжпжп
  //5 : также трай кэч будем юзать еще чтобы все не сломалось к #####
+//ИСПРАВИТЬ ОШИБКИ ОТ ДС. САМАЯ ЛЕВАЯ ВКЛАДКА
