c0968479b335729e4470cfcbde7bc84d4c03b678
samoylovaVD <samoylova349@gmail.com>
2025-11-08 15:56:25 +0500

    Update DogBreed.java

--- src/main/java/parser/DogBreed.java
+++ src/main/java/parser/DogBreed.java
@@ -4,6 +4,7 @@ public class DogBreed{
   
   private String name;
   private String description;
+  private String picture;
 
   public DogBreed(){}
 
@@ -12,4 +13,7 @@ public class DogBreed{
                                     
   public String getDescription() {return description;}
   public void setDescription(String description) { this.description = description;}
+
+  public String getPicture() {return picture;}
+  public void setPicture(String picture) {this.picture = picture;}
 }
