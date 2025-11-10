5a020b5acce04a70640705ba24377080b88b8fd5
samoylovaVD <samoylova349@gmail.com>
2025-11-10 23:59:07 +0500

    Update and rename DogBreedParser.java to DogParser.java

--- src/main/java/parser/DogParser.java
+++ src/main/java/parser/DogParser.java
@@ -4,6 +4,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Document;
 import java.io.IOException;
+import models.DogBreed;
 
 public class DogParser{
   private static final String base_url = "https://www.purina.ru";
@@ -11,9 +12,9 @@ public class DogParser{
 public DogBreed parseDog(String url){
   try{
     Document doc = Jsoup.connect(url).get();
-    DogBreed breed = new DogBreed();
-    
-    Element nameDog = doc.selectFirst("#block-ttt-mainpagecontent > div > div.breed-details > div.component-wrapper > div > div > div > div > div.hero-small--content-area-wrapper > div.hero-small--content-area.hero-small--breed.hero-small--breed__dog > div > h1");
+      models.DogBreed breed = new models.DogBreed();
+
+      Element nameDog = doc.selectFirst("#block-ttt-mainpagecontent > div > div.breed-details > div.component-wrapper > div > div > div > div > div.hero-small--content-area-wrapper > div.hero-small--content-area.hero-small--breed.hero-small--breed__dog > div > h1");
     if (nameDog != null){
       String name = nameDog.text();
         breed.setName(name);
@@ -24,14 +25,14 @@ public DogBreed parseDog(String url){
     Element descriptionDog = doc.selectFirst("#block-ttt-mainpagecontent > div > div.breed-details > div.component-wrapper > div > div > div > div > div.hero-small--content-area-wrapper > div.hero-small--content-area.hero-small--breed.hero-small--breed__dog > div > div.hero-small--content-area-intro > div > p");
     if (descriptionDog != null){
       String description = descriptionDog.text();
-        breed.setDescription(description);
+        breed.setDescript(description);
     }else{
       System.out.println("Не удалось найти описание породы((");}
 
 
     Element picture = doc.selectFirst("#block-ttt-mainpagecontent > div > div.breed-details > div.component-wrapper > div > div > div > div > div.hero-small--image > div > picture > img");
     if (picture != null){
-      breed.setPicture(base_url + picture.attr("src"));
+      breed.setPict(base_url + picture.attr("src"));
     }else{
       System.out.println("Не удалось найти картиночку породы((");}
     return breed;
